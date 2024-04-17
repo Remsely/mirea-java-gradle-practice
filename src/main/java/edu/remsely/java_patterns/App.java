dca94fc95ab72959badd6b2a34ab7595ca555a8c
Remsely <prokopchukromain@mail.ru>
2024-04-17 20:10:54 +0300

    feat: add practise

--- src/main/java/edu/remsely/java_patterns/App.java
+++ src/main/java/edu/remsely/java_patterns/App.java
@@ -1,7 +1,7 @@
-package edu.remsely.java-patterns;
+package edu.remsely.java_patterns;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world from Docker container!");
     }
 }
